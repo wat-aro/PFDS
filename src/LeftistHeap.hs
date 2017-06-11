39488149ed87135f1aab628518b23db5e4fc160d
wat-aro <kazutas1008@gmail.com>
2017-06-11 17:51:59 +0900

    Finish 3.1

--- src/LeftistHeap.hs
+++ src/LeftistHeap.hs
@@ -8,24 +8,49 @@ data LeftistHeap a = E
 
 instance Heap LeftistHeap where
   empty = E
+
   isEmpty E = True
   isEmpty _ = False
+
   insert x = merge (T 1 x E E)
+
   merge left E = left
   merge E left = left
   merge h1@(T _ x a1 b1) h2@(T _ y a2 b2)
     | x < y     = makeT x a1 $ merge b1 h2
     | otherwise = makeT y a2 $ merge h1 b2
+
   findMin (T _ x _ _) = Just x
   findMin _           = Nothing
+
   deleteMin (T _ _ a b) = Just $ merge a b
   deleteMin _ = Nothing
 
-rank :: LeftistHeap a -> Int
+rank :: Ord a => LeftistHeap a -> Int
 rank E = 0
 rank (T r _ _ _) = r
 
-makeT :: a -> LeftistHeap a -> LeftistHeap a -> LeftistHeap a
+makeT :: Ord a => a -> LeftistHeap a -> LeftistHeap a -> LeftistHeap a
 makeT x a b
   | rank a >= rank b = T (rank b + 1) x a b
   | otherwise        = T (rank a + 1) x b a
+
+-- 3.2
+-- わからん
+
+-- 3.3
+fromList :: Ord a => [a] -> LeftistHeap a
+fromLsit' []  = E
+fromList list = fromList' $ fmap (\x -> T 1 x E E) list
+
+fromList' :: Ord a => [LeftistHeap a] -> LeftistHeap a
+fromList' [x]     = x
+fromList' (x:y:xss) = fromList' (xss ++ [merge x y])
+
+toList :: Ord a => LeftistHeap a -> [a]
+toList E = []
+toList (T _ x E E) = [x]
+toList h@(T _ x a b) = x : toList (merge a b)
+
+-- 3.4
+-- わからん
